363db7d2d0c151539b4b3a795a2b511b0b908375
BrunoDM2943 <bdm2943@gmail.com>
2020-05-01 13:34:01 -0300

    Function to use a single interface

--- netmetric.py
+++ netmetric.py
@@ -1,21 +1,28 @@
+#!/usr/bin/python3
+
 import os
 import collections
 import time
+import sys
 
 Interface =  collections.namedtuple('Interface', ['name', 'tx_bytes', 'rx_bytes'])
 
 def read_data():
     interfaces = {}
     for name in os.listdir('/sys/class/net/'):
-        stats_path = '/sys/class/net/{}/statistics'.format(name)
-        tx_bytes = 0
-        rx_bytes = 0
-        with open('{}/rx_bytes'.format(stats_path)) as f:
-            rx_bytes = int(f.readline())
-        with open('{}/tx_bytes'.format(stats_path)) as f:
-            tx_bytes = int(f.readline())
-        interfaces[name] = Interface(name, tx_bytes, rx_bytes)
+        interfaces[name] = read_interface(name)
     return interfaces
+
+def read_interface(name):
+    stats_path = '/sys/class/net/{}/statistics'.format(name)
+    tx_bytes = 0
+    rx_bytes = 0
+    with open('{}/rx_bytes'.format(stats_path)) as f:
+        rx_bytes = int(f.readline())
+    with open('{}/tx_bytes'.format(stats_path)) as f:
+        tx_bytes = int(f.readline())
+    return Interface(name, tx_bytes, rx_bytes)
+
 
 def printer(interface):
     text = 'Interface:{}\tUpload:{}\tDownload:{}\t' 
@@ -41,10 +48,26 @@ def check_diff(old, new):
     rx_bytes = new.rx_bytes - old.rx_bytes
     return Interface(new.name, tx_bytes, rx_bytes)
 
-while(True):
-    interfaces = read_data()
-    time.sleep(1)
-    new_interfaces = read_data() 
-    os.system('clear')
-    for key in new_interfaces.keys():
-        printer(check_diff(interfaces[key], new_interfaces[key]))
+def watch_all():
+    while(True):
+        interfaces = read_data()
+        time.sleep(1)
+        new_interfaces = read_data() 
+        os.system('clear')
+        for key in new_interfaces.keys():
+            printer(check_diff(interfaces[key], new_interfaces[key]))
+
+def watch_one(name):
+    while(True):
+        interface = read_interface(name)
+        time.sleep(1)
+        new_interface = read_interface(name)
+        os.system('clear')
+        printer(check_diff(interface, new_interface))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+       watch_one(sys.argv[1])
+    else:
+        watch_all()
